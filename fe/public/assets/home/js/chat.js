5b3941d7b20a9eb5dc813a1d94576d4b847a3515
duclong3907 <dlongminhtan@gmail.com>
2024-04-20 14:24:37 +0700

    Fix interface chat home

---
+++ fe/public/assets/home/js/chat.js
@@ -0,0 +1,24 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = null; // Variable to store user's message
+const inputInitHeight = chatInput.scrollHeight;
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim();
+    if (!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+
+    setTimeout(() => {
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+    }, 100);
+}
+
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
--- fe/src/pages/Chat/ChatHome.jsx
+++ fe/src/pages/Chat/ChatHome.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { createConnection, sendMessage, registerMessageReceivedHandler } from '../../Service/signalRChat';
-import { Modal, Button, Form, ListGroup } from 'react-bootstrap';
-import styled from 'styled-components';
 import CryptoJS from 'crypto-js';
-
+import './Chat.css'
+import { Helmet } from 'react-helmet';
+import Icon from '@mdi/react';
+import { mdiClose, mdiMessageText, mdiRobotAngryOutline, mdiSend } from '@mdi/js';
 const ChatHome = ({ username }) => {
     const [messages, setMessages] = useState(JSON.parse(CryptoJS.AES.decrypt(localStorage.getItem(username) || '', 'secret key').toString(CryptoJS.enc.Utf8) || '[]'));
     const [newMessage, setNewMessage] = useState("");
-    const [show, setShow] = useState(false);
+    const [isWaitingResponse, setIsWaitingResponse] = useState(false);
 
     useEffect(() => {
         createConnection(username);
         registerMessageReceivedHandler((sender, message) => {
             if (sender === username || sender === 'Admin') {
+                setIsWaitingResponse(false); // Set to false when a message is received
                 setMessages(messages => {
                     const newMessages = [...messages, { sender, message }];
                     localStorage.setItem(username, CryptoJS.AES.encrypt(JSON.stringify(newMessages), 'secret key').toString());
@@ -24,6 +26,7 @@ const ChatHome = ({ username }) => {
 
     const handleSend = () => {
         sendMessage(username, 'Admin', newMessage);
+        setIsWaitingResponse(true); // Set to true when a message is sent
         setMessages(messages => {
             const newMessages = [...messages, { sender: username, message: newMessage }];
             localStorage.setItem(username, CryptoJS.AES.encrypt(JSON.stringify(newMessages), 'secret key').toString());
@@ -32,47 +35,58 @@ const ChatHome = ({ username }) => {
         setNewMessage("");
     }
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const StyledButton = styled(Button)`
-        position: fixed;
-        right: 0;
-        bottom: 0;
-        margin-bottom: 10rem;
-        `;
     return (
         <div>
-            <StyledButton variant="primary" onClick={handleShow}>
-                Chat
-            </StyledButton>
-
-            <Modal show={show} onHide={handleClose} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>Chat with Admin</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <ListGroup>
-                        {messages.map((message, index) => (
-                            <p key={index} className={message.sender === username ? 'text-left' : 'text-right'}>
-                                <strong>{message.sender}:</strong> {message.message}
-                            </p>
-                        ))}
-                    </ListGroup>
-                    <Form>
-                        <Form.Group controlId="messageInput">
-                            <Form.Label>Message</Form.Label>
-                            <Form.Control type="text" value={newMessage} onChange={e => setNewMessage(e.target.value)} placeholder="Enter Message" />
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="primary" onClick={handleSend}>
-                        Send
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            <button className="chatbot-toggler">
+                <span className="material-symbols-rounded"><Icon path={mdiMessageText} size={1} /></span>
+                <span className="material-symbols-outlined"><Icon path={mdiClose} size={1} /></span>
+            </button>
+            <div className="chatbot">
+                <header>
+                    <h2>Chat With Admin</h2>
+                    <span className="close-btn material-symbols-outlined">close</span>
+                </header>
+                <ul className="chatbox">
+                    <li className="chat incoming">
+                        <span className="material-symbols-outlined"><Icon path={mdiRobotAngryOutline} size={1} /></span>
+                        <p>Hi there 👋<br />How can I help you today?</p>
+                    </li>
+                    {messages.map((message, index) => (
+                        <li key={index} className={message.sender === username ? 'chat outgoing' : 'chat incoming'}>
+                            <span className="material-symbols-outlined">{message.sender === 'Admin' ? <Icon path={mdiRobotAngryOutline} size={1} /> : ''}</span>
+                            <p>{message.message}</p>
+                        </li>
+                    ))}
+                    {isWaitingResponse && (
+                        <li className="chat incoming">
+                            <span className="material-symbols-outlined"><Icon path={mdiRobotAngryOutline} size={1} /></span>
+                            <p>Waiting...</p>
+                        </li>
+                    )}
+                </ul>
+                <div className="chat-input">
+                    <textarea
+                        placeholder="Enter a message..."
+                        required
+                        defaultValue={""}
+                        type="text"
+                        value={newMessage}
+                        onChange={e => setNewMessage(e.target.value)}
+                        onKeyDown={e => {
+                            if (e.key === "Enter" && !e.shiftKey) {
+                                e.preventDefault();
+                                handleSend();
+                            }
+                        }}
+                    />
+                    <span onClick={handleSend} ><Icon path={mdiSend} size={1} /></span>
+                </div>
+            </div>
+            <Helmet>
+                <script src="assets/home/js/chat.js"></script>
+            </Helmet>
         </div>
+
     );
 }
 
